3b745fe812ab5d1e8a291f92e6f44328e420e58d
michael-bailey <mickyb18a@gmail.com>
2025-03-17 14:43:28 +0000

    Created snazzy block allocation progress bar.

--- FileStorageService.www/Models/FileHandleListModel.cs
+++ FileStorageService.www/Models/FileHandleListModel.cs
@@ -1,6 +1,11 @@
+using FileStorageService.www.Data;
+
 namespace FileStorageService.www.Models;
 
 public class FileHandleListModel
 {
-	public List<FileHandleModel> FileHandles { get; init; }
+	public required List<FileHandleModel> FileHandles { get; init; }
+	public int CurrentBlockCount => FileHandles.Select(s => s.BlockCount).Sum();
+	public int AllocatedBlockCount => 1024;
+	public int BlockUsagePercentage => (int)Math.Ceiling(((float)CurrentBlockCount / AllocatedBlockCount)*100);
 }
